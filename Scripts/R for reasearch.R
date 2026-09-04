f822b42fc436f855b74b35a45867562c8bce2fdc
Arafath Chowdhury <chowdhuryarafath420@gmail.com>
2026-09-04 23:44:49 +0600

    code

--- Scripts/R for reasearch.R
+++ Scripts/R for reasearch.R
@@ -1,1 +1,29 @@
-age <- 1
+# checking reserved code
+?reserved
+
+age <- 10
+my_age_is <- 24
+
+Name <- "jone dram"
+class(Name)
+
+num1 <- 12
+num2 <- 2
+num1 + num2
+num1 - num2 
+num1 * num2
+num1 / num2
+
+
+num1 %% num2
+
+message <- "hello world"
+
+print(message)
+
+age * 2
+age / 2
+
+!age
+
+
